ed45e8cbbe2ae7609cc47dfd1123ebdcb4bb619a
Rriftt <rriftt.arch@proton.me>
2026-03-11 23:54:30 +0530

    Added docs/memory/RaiArena.md

--- rriftt_ai.h
+++ rriftt_ai.h
@@ -308,19 +308,25 @@ Please see this section of the header for all overrideable libc dependencies. Yo
 ```c */
 float rai_randn(float mean, float std_dev);
 /* ```
-* Pseudorandom numbers drawn from a Gaussian distribution centred around `mean` and having standard deviation `std_dev`.
-* Seeded by `RAI_SRAND()`.
-* Depends on `RAI_RAND()` for uniformly distributed pseudorandom number generation.
-* Employs **Box-Muller transform** on the uniform random numbers to generate normally distributed random numbers.
+ * Pseudorandom numbers drawn from a Gaussian distribution centred around `mean` and having standard deviation `std_dev`.
+ * Seeded by `RAI_SRAND()`.
+ * Depends on `RAI_RAND()` for uniformly distributed pseudorandom number generation.
+ * Employs **Box-Muller transform** on the uniform random numbers to generate normally distributed random numbers.
 ``` */
 
 // ------------------------- Arena API ---------------------------------
-// Arena Allocator
+/* ```markdown docs/memory/RaiArena.md
+```c */
 typedef struct {
 	size_t alloced_bytes;
 	size_t capacity_bytes;
 	void* memory_begin;
 } RaiArena;
+/* ```
+ * Arena Allocator / Linear Allocator / Bump Allocator / Region Allocator
+ * Used for requesting a chunk of memory using `RAI_MALLOC()` and allocating linearly inside it.
+ * `rriftt_ai.h` uses `RaiArena` for every allocation. A function/macro that allocates memory can be readily identified from the fact that it will accept `RaiArena *` as its first parameter.
+``` */
 
 RaiArena rai_arena_create(size_t capacity_bytes);
 void rai_arena_destroy(RaiArena* arena);

5e39a656f24af7a05a79e9b283de7ac8be63fb7c
Rriftt <rriftt.arch@proton.me>
2026-03-11 10:48:04 +0530

    Added docs/rai_randn.md

--- rriftt_ai.h
+++ rriftt_ai.h
@@ -312,7 +312,16 @@ Please see this section of the header for all overrideable libc dependencies.
 #endif // RAI_INFINITY
 
 // ------------------------- Math API ---------------------------------
-// PRNG - Gaussian Distribution
+#ifndef RAI__FILE_DOCS_RAI_RANDN_MD
+/*
+# float rai_randn(float mean, float std_dev);
+
+* Pseudorandom numbers drawn from a Gaussian distribution centred around `mean` and having standard deviation `std_dev`.
+* Seeded by `RAI_SRAND()`.
+* Depends on `RAI_RAND()` for uniformly distributed pseudorandom number generation.
+* Employs **Box-Muller transform** on the uniform random numbers to generate normally distributed random numbers.
+*/
+#endif // RAI__FILE_DOCS_RAI_RANDN_MD
 float rai_randn(float mean, float std_dev);
 
 // ------------------------- Arena API ---------------------------------
